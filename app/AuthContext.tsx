0c9af254e311c5ffb9bb0cef23d3d5a789e85fee
WesleyCodeMast <wesleyleck92@outlook.com>
2025-03-30 21:03:39 -0400

    add space...

--- app/AuthContext.tsx
+++ app/AuthContext.tsx
@@ -6,6 +6,7 @@ export interface AuthContextProps {
   children: React.ReactNode;
 }
 
+
 export default function AuthContext({ children }: AuthContextProps) {
   return <SessionProvider>{children}</SessionProvider>;
 }
